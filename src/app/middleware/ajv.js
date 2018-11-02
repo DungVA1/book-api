bbffee3c2e14830c6a3bce16ed37b75d203cf0fb
Dung Vu Anh <dungvua@HarveyNash.vn.local>
2018-11-02 10:45:47 +0700

    Exception handler

--- src/app/middleware/ajv.js
+++ src/app/middleware/ajv.js
@@ -2,9 +2,10 @@ import AJV from 'ajv';
 import fs from 'fs-extra';
 import path from 'path';
 import logger from '../../lib/common/logger';
-import { UNPROCESSABLE_ENTITY, EXCEPTION } from '../../constant/common/error-message';
+import { UNPROCESSABLE_ENTITY } from '../../constant/common/error-message';
 import { statusCode } from '../../constant/common/status-code';
 import definition from '../schema/definition.json';
+import exception from '../../constant/common/exception';
 
 /**
  * @author Dung Vu Anh
@@ -28,6 +29,7 @@ export const validateBody = async (req, res, next) => {
     const valid = ajv.validate(JSON.parse(schemaObj), req.body);
     if (!valid) {
       return res.status(statusCode.UNPROCESSABLE_ENTITY).json({
+        status: statusCode.UNPROCESSABLE_ENTITY,
         error: true,
         message: UNPROCESSABLE_ENTITY,
         messageCode: 'UNPROCESSABLE_ENTITY',
@@ -38,10 +40,6 @@ export const validateBody = async (req, res, next) => {
     next();
   } catch (ex) {
     logger.error(`[${__dirname}]: ${JSON.stringify(ex)}`);
-    res.status(statusCode.EXCEPTION).json({
-      error: true,
-      message: EXCEPTION,
-      messageCode: 'EXCEPTION',
-    });
+    res.status(statusCode.EXCEPTION).json(exception);
   }
 };
---
+++ src/constant/common/exception.js
@@ -0,0 +1,9 @@
+import { statusCode } from './status-code';
+import { EXCEPTION } from './error-message';
+
+export default {
+  status: statusCode.EXCEPTION,
+  error: true,
+  message: EXCEPTION,
+  messageCode: 'EXCEPTION',
+};
