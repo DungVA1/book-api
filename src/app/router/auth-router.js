0811a1c2a89dc3206d850f4c2b336a95b5903471
Dung Vu Anh <dungvua@HarveyNash.vn.local>
2018-11-09 14:50:27 +0700

    REMOVE

--- src/app/router/auth-router.js
+++ src/app/router/auth-router.js
@@ -6,25 +6,7 @@ export const authRoute = express.Router();
 
 // List routers of auth api should be define in array routers
 const routers = [
-  /**
-    * This function to authenticate user
-    * @route POST /login
-    * @group auth - Operations about authenticate
-    * @param {string} account.body.required - username or email
-    * @param {string} password.body.required - user's password.
-    * @returns {object} 200 - An array of user info
-    * @returns {Error}  default - Unexpected error
-  */
   '/login',
-  /**
-    * This function comment is parsed by doctrine
-    * @route GET /api
-    * @group auth - Operations about authenticate
-    * @param {string} email.query.required - username or email
-    * @param {string} password.query.required - user's password.
-    * @returns {object} 200 - An array of user info
-    * @returns {Error}  default - Unexpected error
-  */
   '/register',
   '/change-password',
 ];
