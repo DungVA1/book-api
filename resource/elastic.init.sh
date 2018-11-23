002e5a7acbb3c156cb94ee04150b0b8b00b6e4dc
Dung Vu Anh <dungvua@HarveyNash.vn.local>
2018-11-23 17:14:09 +0700

    Init data

--- resource/elastic.init.sh
+++ resource/elastic.init.sh
@@ -13,8 +13,12 @@ fi
 
 echo Lets go http://$ES_HOST:9200
 
+# Init schema
 curl -X PUT -H "Content-Type: application/json" $ES_HOST:9200/user -d @"./resource/user.mapping.json"
 curl -X PUT -H "Content-Type: application/json" $ES_HOST:9200/post -d @"./resource/post.mapping.json"
 curl -X PUT -H "Content-Type: application/json" $ES_HOST:9200/category -d @"./resource/category.mapping.json"
 curl -X PUT -H "Content-Type: application/json" $ES_HOST:9200/tag -d @"./resource/tag.mapping.json"
 curl -X PUT -H "Content-Type: application/json" $ES_HOST:9200/role -d @"./resource/role.mapping.json"
+
+# Init data
+curl -X PUT -H "Content-Type: application/json" $ES_HOST:9200/user/user -d @"./resource/user.data.json"
