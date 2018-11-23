6c24b8fc34756823749484379eb678a873ee2803
Dung Vu Anh <dungvua@HarveyNash.vn.local>
2018-11-23 10:49:34 +0700

    Swagger

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from './swagger/swagger.json';
 import logger from '../lib/common/logger';
 import { verify } from './middleware/jwt';
 import {
@@ -8,48 +10,22 @@ import {
   authRoute,
 } from './router';
 import packageJson from '../../package.json';
-import expressSwagger from 'express-swagger-generator';
 
 const baseUrl = process.env.BASE_URL || '/api';
 const version = `v${(packageJson.version.split('.')[0] || 1)}`;
-let options = {
-  swaggerDefinition: {
-      info: {
-          description: 'This is blog document api swagger',
-          title: 'Swagger',
-          version: packageJson.version,
-      },
-      host: 'localhost:3000',
-      basePath: `${baseUrl}/${version}`,
-      produces: [
-          'application/json',
-      ],
-      schemes: ['http', 'https'],
-      securityDefinitions: {
-          JWT: {
-              type: 'apiKey',
-              in: 'header',
-              name: 'x-access-token',
-              description: '',
-          },
-      },
-  },
-  basedir: __dirname, //app absolute path
-  files: ['./router/*.js'], //Path to the API handle folder
-};
 
 export const initExpressApi = () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded());
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
   app.use(/^((?!login|register).)*$/, verify);
   app.use(`${baseUrl}/${version}`, [
     bookRoute,
     userRoute,
     authRoute,
   ]);
-  expressSwagger(app)(options);
   app.listen(process.env.EXPRESS_PORT || 3000, () => {
     logger.info('LISTENNING AT 3000');
   });
