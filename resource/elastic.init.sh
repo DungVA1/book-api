a0eb6d260d905e19068c2b0c455d23d584948ac6
Dung Vu Anh <dungvua@HarveyNash.vn.local>
2018-11-23 14:56:04 +0700

    Improve

--- resource/elastic.init.sh
+++ resource/elastic.init.sh
@@ -1,14 +1,20 @@
-echo 'Wait me 3 seconds'
+HOST='localhost'
 
-for i in {1..3}
-  do
-    sleep 1
-    echo 'Sleeping' $i's'
-  done
+if [[ $IS_DOCKER == 'true' ]];
+then
+  echo 'Detected is docker enviroment'
+  HOST='elasticsearch'
+  echo 'Wait me 3 seconds'
+  for i in {1..3}
+    do
+      sleep 1
+      echo 'Sleeping' $i's'
+    done
+fi
 
 echo 'Lets go'
-curl -X PUT -H "Content-Type: application/json" elasticsearch:9200/user -d @"./resource/user.mapping.json"
-curl -X PUT -H "Content-Type: application/json" elasticsearch:9200/post -d @"./resource/post.mapping.json"
-curl -X PUT -H "Content-Type: application/json" elasticsearch:9200/category -d @"./resource/category.mapping.json"
-curl -X PUT -H "Content-Type: application/json" elasticsearch:9200/tag -d @"./resource/tag.mapping.json"
-curl -X PUT -H "Content-Type: application/json" elasticsearch:9200/role -d @"./resource/role.mapping.json"
+curl -X PUT -H "Content-Type: application/json" $HOST:9200/user -d @"./resource/user.mapping.json"
+curl -X PUT -H "Content-Type: application/json" $HOST:9200/post -d @"./resource/post.mapping.json"
+curl -X PUT -H "Content-Type: application/json" $HOST:9200/category -d @"./resource/category.mapping.json"
+curl -X PUT -H "Content-Type: application/json" $HOST:9200/tag -d @"./resource/tag.mapping.json"
+curl -X PUT -H "Content-Type: application/json" $HOST:9200/role -d @"./resource/role.mapping.json"
