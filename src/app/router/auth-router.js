7a790252feb666c728e6456e697e45b93d3f9d5a
DungVA <dungva1505@gmail.com>
2018-12-13 23:18:06 +0700

    BackUp

--- src/app/router/auth-router.js
+++ src/app/router/auth-router.js
@@ -1,14 +1,8 @@
 import express from 'express';
 import { validateBody } from '../middleware/ajv';
-import authHandlers from '../../handler/auth';
+import { login, register } from '../../handler/auth';
 
 export const authRoute = express.Router();
 
-// List routers of auth api should be define in array routers
-const routers = [
-  '/login',
-  '/register',
-  '/change-password',
-];
-
-authRoute.post(routers, validateBody, authHandlers);
+authRoute.post('/login/:id', validateBody, login);
+authRoute.post('/register', validateBody, register);
